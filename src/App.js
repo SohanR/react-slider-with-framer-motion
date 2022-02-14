b17a39924af86e18e9ecfda2f639ac05d2602a10
sohanr <mizan.rahman66d@gmail.com>
2022-02-15 00:50:59 +0600

    drag

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
+import { motion } from 'framer-motion';
 import './App.css';
+import images from './images';
+//import {useRef, useState, useEffect } from 'react';
 
 function App() {
+  console.log(images);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <motion.div className="carousel" >
+       <motion.div className="inner-carousel" drag='x' >
+         {images.map(image =>{
+           return(
+             <motion.div className='item' >
+               <img src={image} alt="" />
+             </motion.div>
+           )
+         })}
+       </motion.div>
+     </motion.div>
     </div>
   );
 }
